40a0c1a6ccbdc6eb6ea7295ca4d501b46b2b0d15
Luiz Fernando <lfaires@gmail.com>
2021-05-26 00:13:18 -0300

    estados para renderizar mensagens de loading e erro

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import styled from 'styled-components'
 import { useState, useContext, useEffect } from 'react';
 import { Container, Posts, Trending } from "../styledComponents/Content";
 import Post from './Post';
@@ -8,22 +9,26 @@ import UserContext from "../contexts/UserContext";
 export default function Timeline(){
     {/*const {user} = useContext(UserContext)*/};
     const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
 
     useEffect(() => {loadingPosts()},[])
     
     function loadingPosts(){
+        setIsLoading(true)
         const config = { headers:{ Authorization: `Bearer 732249a2-af53-4731-bd8f-4a7c79b3015a`}};        const request = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts', config)
 
-        request.then( response => {setPosts(response.data.posts); console.log(response.data.posts)})
-        request.catch( () => alert("Houve uma falha ao obter os posts, por favor atualize a página"))
+        request.then( response => {setPosts(response.data.posts); setIsLoading(false)})
+        request.catch( () => setIsError(true))
     }
 
     return(
-        
         <Container>
             <h1>timeline</h1>
             <div>
                 <Posts>
+                    { isLoading ? <Load>Loading</Load> : ""}
+                    { isError ? <Load>Houve uma falha ao obter os posts, por favor atualize a página</Load> : ""}
                     {posts.map( post => <Post key={post.id} post={post}/>)}
                 </Posts>
                 <Trending >
@@ -34,3 +39,10 @@ export default function Timeline(){
         </Container>
     )
 }
+
+const Load = styled.div`
+    display: flex;
+    justify-content: center;
+    color: #FFF;
+    font-size: 30px;
+`
