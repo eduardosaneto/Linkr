ceff1531c79b8d63112db158bdb421cf47c8f41b
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-05-26 18:06:12 -0300

    limpeza do código

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -45,7 +45,6 @@ export default function Timeline(){
                         { isLoading ? <Load>Loading</Load> : ""}
                         { isError ? <Load>Houve uma falha ao obter os posts, <br/> por favor atualize a página</Load> : ""}
                         { isEmpty && !isLoading ? <Load>Nenhum post encontrado</Load> : ""}
-                        {/*Caixa para publicar post*/}
                         {posts.map( post => <Post key={post.id} post={post} user={post.user}/>)}
                     </Posts>
                     <Trending >
