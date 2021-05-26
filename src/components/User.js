6b4a912ead6107fdc8d0c6060d95c3ee9e3e9cc7
Luiz Fernando <lfaires@gmail.com>
2021-05-26 18:12:09 -0300

    componentização user.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,62 @@
+import axios from 'axios'
+import { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Container, Posts, Trending } from "../styledComponents/Content";
+import styled from 'styled-components'
+
+import Usercontext from '../contexts/UserContext'
+import TrendingBar from './TrendingBar';
+import Post from './Post'
+
 export default function User(){
+    const { user } = useContext(Usercontext)
+    const {id} = useParams()
+    const [userPosts, setUserPosts] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
+    const [isEmpty, setIsEmpty] = useState(false)
+
+    useEffect(() => loadingPostsUser(),[])
+    
+    function loadingPostsUser() {
+        setIsLoading(true)
+        setIsError(false)
+        const config = { headers: { Authorization: `Bearer ${user.token}`}}
+        const request = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/${id}/posts`, config)
+
+        request.then( response => {
+            const data = response.data.posts
+            setUserPosts([...response.data.posts])
+            setIsLoading(false)
+            if(userPosts === data) {
+                setIsEmpty(true)
+            } else {
+                setIsEmpty(false)
+            }})
+    }
+
     return(
-        <div>User</div>
+        <Container>
+            <h1>Luiz's posts</h1>
+            <div>
+                <Posts>
+                    { isLoading ? <Load>Loading</Load> : ""}
+                    { isError ? <Load>Houve uma falha ao obter os posts, <br/> por favor atualize a página</Load> : ""}
+                    { isEmpty && !isLoading ? <Load>Nenhum post encontrado</Load> : ""}
+                    {userPosts.map( post => <Post key={post.id} post={post} user={post.user}/>)}
+                </Posts>
+                <Trending >
+                    <TrendingBar/>
+                </Trending>
+            </div>
+        </Container>
     )
 }
+
+const Load = styled.div`
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    color: #FFF;
+    font-size: 30px;
+`
