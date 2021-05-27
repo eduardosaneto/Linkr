eaf11ce83f89af91696fcafb9459ffa63a34d109
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-05-26 23:11:51 -0300

    Acertando a CreatePosts

--- src/components/CreatePosts.js
+++ src/components/CreatePosts.js
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 import { useContext, useState } from "react";
 import axios from "axios";
-
-export default function MyPosts() {
+import Usercontext from "../contexts/UserContext";
+export default function CreatePosts() {
   const [link, setLink] = useState("");
   const [text, setText] = useState("");
   const [isDisabled, setIsDisabled] = useState(false);
-  const token = '732249a2-af53-4731-bd8f-4a7c79b3015a';
+  const {user} = useContext(Usercontext);
+  const image = user.user.avatar;
+
+
 
   function Submit(event) {
     event.preventDefault();
@@ -15,48 +18,35 @@ export default function MyPosts() {
     } else {
       const body = {
         text,
-	      link
+        link
       };
-
       const config = {
         headers: {
-          Authorization: `Bearer ${token}`
+          Authorization: `Bearer ${user.token}`
         },
       };
-
       const request = axios.post(
         "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts",
         body,
         config
       );
-
       setIsDisabled(true);
-
       request.then((response) => {
          setIsDisabled(false);
          setLink("");
          setText("");
       });
-
       request.catch(() => {
         alert("Houve um erro ao publicar seu link");
         setIsDisabled(false);
       });
     }
-
   }
-
   return (
     <Post>
-      <Photo>
-        <img
-          src='https://static1.purebreak.com.br/articles/4/96/74/4/@/380748--mulher-maravilha-3-ira-encerrar-histor-300x254-1.jpg'
-          alt={"imageofuser"}
-        />
-      </Photo>
-      
+      <Photo image={image}></Photo>
         <Form>
-          <h1>O que você tem pra favoritar hoje?</h1>
+          <h2>O que você tem pra favoritar hoje?</h2>
           <Styledinput
             type='link'
             disabled={isDisabled? true : false}
@@ -91,35 +81,27 @@ export default function MyPosts() {
     </Post>
   );
 }
-
 const Post = styled.div`
   width: 611px;
   height: 209px;
-  background: #ffffff;
-  font-family: "Lato", sans-serif;
+  background: #FFFFFF;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 16px;
-  margin-top: 60px;
   padding: 15px;
   display: flex;
   position: relative;
   font-weight: 300;
   font-size: 23px;
   color: #707070;
-
-  h1 {
-    margin-bottom: 12px;
-  }
+  margin-bottom: 20px;
 `;
-
 const Photo = styled.div`
   width: 50px;
   height: 50px;
   border-radius: 100px;
   background-size: cover;
   margin:5px 0px 0px 5px ;
-  background-image: url("https://static1.purebreak.com.br/articles/4/96/74/4/@/380748--mulher-maravilha-3-ira-encerrar-histor-300x254-1.jpg");
-
+  background-image: url(${(props) => props.image});
   img {
     width: 50px;
     height: 50px;
@@ -128,21 +110,20 @@ const Photo = styled.div`
 `;
 const Form = styled.form`
   display: flex;
-  justify-content: center;
   flex-direction: column;
-  position: fixed;
-  margin-left: 70px;
+  margin-left: 20px;
   margin-top: 5px;
+  h2 {
+    margin-bottom: 12px;
+  }
 `;
-
-
 const Styledinput = styled.input`
   width: 502px;
   height: 30px;
   font-family: "Lato", sans-serif;
   font-size: 15px;
   line-height: 18px;
-  background: #efefef;
+  background: #EFEFEF;
   border-radius: 5px;
   color: #707070;
   padding-left: 10px;
@@ -151,14 +132,13 @@ const Styledinput = styled.input`
   margin-bottom: 5px;
   font-weight: 300;
 `;
-
 const StyledinputText = styled.input`
   width: 502px;
   height: 66px;
   font-family: "Lato", sans-serif;
   font-size: 15px;
   line-height: 18px;
-  background: #efefef;
+  background: #EFEFEF;
   border-radius: 5px;
   color: #707070;
   padding-left: 10px;
@@ -169,21 +149,183 @@ const StyledinputText = styled.input`
   display: flex;
   justify-content: start;
 `;
-
 const Button = styled.button`
   position: absolute;
   width: 112px;
   height: 31px;
-  left: 389px;
-  top: 140px;
-  background: #1877f2;
+  left: 480px;
+  top: 160px;
+  background: #1877F2;
   border-radius: 5px;
   outline-color: transparent;
   line-height: 17px;
-  color: #ffffff;
+  color: #FFFFFF;
   margin-bottom: 10px;
   border: none;
-  font-family: "Lato", sans-serif;
   font-weight: bold;
 `;
+23h09
+import styled from "styled-components";
+import { useContext, useState } from "react";
+import axios from "axios";
+import Usercontext from "../contexts/UserContext";
+export default function CreatePosts() {
+  const [link, setLink] = useState("");
+  const [text, setText] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
+  const {user} = useContext(Usercontext);
+  const image = user.user.avatar;
 
+  function Submit(event) {
+    event.preventDefault();
+    if (link.length === 0) {
+      alert("Percebemos que você não preencheu o link corretamente... Tente novamente ;)");
+    } else {
+      const body = {
+        text,
+        link
+      };
+      const config = {
+        headers: {
+          Authorization: Bearer ${user.token}
+        },
+      };
+      const request = axios.post(
+        "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts",
+        body,
+        config
+      );
+      setIsDisabled(true);
+      request.then((response) => {
+         setIsDisabled(false);
+         setLink("");
+         setText("");
+      });
+      request.catch(() => {
+        alert("Houve um erro ao publicar seu link");
+        setIsDisabled(false);
+      });
+    }
+  }
+  return (
+    <Post>
+      <Photo image={image}></Photo>
+        <Form>
+          <h2>O que você tem pra favoritar hoje?</h2>
+          <Styledinput
+            type='link'
+            disabled={isDisabled? true : false}
+            onChange={(e) => setLink(e.target.value)}
+            value={link}
+            placeholder='http://...'
+            isDisabled={isDisabled}
+            required></Styledinput>
+          <StyledinputText
+            type='text'
+            disabled={isDisabled? true : false}
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+            placeholder='O que você tem a dizer sobre isso?'
+            isDisabled={isDisabled}
+            required></StyledinputText>
+          <Button 
+          isDisabled={isDisabled}
+          onClick={
+               isDisabled
+                 ? ""
+                 : 
+                    (event) => Submit(event)
+            }>
+            <span> {isDisabled ? (
+                "Publicando"
+              ) : (
+                "Publicar"
+              )}</span>
+          </Button>
+        </Form>
+    </Post>
+  );
+}
+const Post = styled.div`
+  width: 611px;
+  height: 209px;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 16px;
+  padding: 15px;
+  display: flex;
+  position: relative;
+  font-weight: 300;
+  font-size: 23px;
+  color: #707070;
+  margin-bottom: 20px;
+`;
+const Photo = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 100px;
+  background-size: cover;
+  margin:5px 0px 0px 5px ;
+  background-image: url(${(props) => props.image});
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 100px;
+  }
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+  margin-top: 5px;
+  h2 {
+    margin-bottom: 12px;
+  }
+`;
+const Styledinput = styled.input`
+  width: 502px;
+  height: 30px;
+  font-family: "Lato", sans-serif;
+  font-size: 15px;
+  line-height: 18px;
+  background: #EFEFEF;
+  border-radius: 5px;
+  color: #707070;
+  padding-left: 10px;
+  border: none;
+  outline-color: transparent;
+  margin-bottom: 5px;
+  font-weight: 300;
+`;
+const StyledinputText = styled.input`
+  width: 502px;
+  height: 66px;
+  font-family: "Lato", sans-serif;
+  font-size: 15px;
+  line-height: 18px;
+  background: #EFEFEF;
+  border-radius: 5px;
+  color: #707070;
+  padding-left: 10px;
+  border: none;
+  outline-color: transparent;
+  margin-bottom: 10px;
+  font-weight: 300;
+  display: flex;
+  justify-content: start;
+`;
+const Button = styled.button`
+  position: absolute;
+  width: 112px;
+  height: 31px;
+  left: 480px;
+  top: 160px;
+  background: #1877F2;
+  border-radius: 5px;
+  outline-color: transparent;
+  line-height: 17px;
+  color: #FFFFFF;
+  margin-bottom: 10px;
+  border: none;
+  font-weight: bold;
+`;
