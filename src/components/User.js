5bcd2574e6206b130f674a96c34ade7fb604ecff
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-05-31 20:32:03 -0300

    Começano a feature, ja adicionado o botao follow/unfollow(sem funcionalidade)

--- src/components/User.js
+++ src/components/User.js
@@ -10,43 +10,89 @@ import Post from './Post';
 
 export default function User(){
     const {user, setUser} = useContext(Usercontext);
-    const {id} = useParams()
-    const [userPosts, setUserPosts] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
-    const [isError, setIsError] = useState(false)
-    const [isEmpty, setIsEmpty] = useState(false)
+    const {id} = useParams();
+    const [userPosts, setUserPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(false);
+    const [followList, setFollowList] = useState([]);
+    const [userInfo, setUserInfo] = useState(undefined);
+
+    const localstorage = JSON.parse(localStorage.user);
+    const token = localstorage.token;
+    const config = { headers: { Authorization: `Bearer ${token}`}};
+
     
-    useEffect(() => loadingPostsUser(),[])
+    useEffect(() =>{
+        loadingUser();
+        loadingPostsUser();
+        FollowList();
+    },[]);
     
     function username() {
-        return userPosts[0].user.username +"'s posts"
+        //return userPosts[0].user.username +"'s posts";
+        return userInfo.username +"'s posts";
     }
 
     function loadingPostsUser() {
-        const localstorage = JSON.parse(localStorage.user);
-        const token = localstorage.token;
-        setIsLoading(true)
-        setIsError(false)
-        const config = { headers: { Authorization: `Bearer ${token}`}}
+        setIsLoading(true);
+        setIsError(false);
         const request = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/${id}/posts`, config)
 
         request.then( response => {
             setUser(localStorage.user);
-            const data = response.data.posts
-            setUserPosts([...response.data.posts])
-            setIsLoading(false)
+            const data = response.data.posts;
+            setUserPosts([...response.data.posts]);
+            setIsLoading(false);
             if(userPosts === data) {
-                setIsEmpty(true)
+                setIsEmpty(true);
             } else {
-                setIsEmpty(false)
+                setIsEmpty(false);
             }})
     }
+
+    function FollowList(){
+        const request = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/follows",config);
+        request.then((e)=>{
+            setFollowList(e.data.users);
+        })
+
+    }
+
+    function CheckIfIFollow(){
+        if(!userInfo){
+            return
+        }
+        const idList = followList.map((e)=>e.id);
+        return idList.includes(id);
+    }
+
+    function loadingUser(){
+        
+        const request = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/${id}`,config);
+        request.then((e)=>{
+            setUserInfo(e.data.user);
+            console.log(e.data);
+            console.log(e.data.user);
+        })
+        request.catch((e)=>{
+            console.log(e.data.response);
+            console.log('nao consegui');
+        })
+    }
+
 
     return(
         <>
             <Navbar />
             <Container>
-                <h1>{ userPosts.length === 0 ? "" : username()}</h1>
+                <TitleContainer>
+                    <Title>
+                    {userInfo ? <img src={`${userInfo.avatar}`} alt={`${userInfo.username} profile`}/> : ""} 
+                        <h1>{ userInfo ? username() : ""}</h1>
+                    </Title>
+                    <Button follow={CheckIfIFollow()}> {CheckIfIFollow()?'Unfollow':'Follow'} </Button>
+                </TitleContainer>
                 <div>
                     <Posts>
                         { isLoading ? <Load>Loading</Load> : ""}
@@ -62,3 +108,28 @@ export default function User(){
         </>
     )
 }
+
+const TitleContainer = styled.div`
+    align-items: center !important;
+`
+const Title=styled.div`
+    display: flex;
+    justify-content: flex-start !important;
+    align-items: center !important;
+    img {
+        border-radius: 50%;
+        width: 50px;
+        height: 50px;
+        margin-right: 18px;
+    }
+`
+const Button = styled.button`
+    width: 112px;
+    height: 31px;
+    border: none;
+    border-radius: 5px;
+    background-color: ${props=>props.follow?"#FFF":"#1877F2"};
+    color:${props=>props.follow?"#1877F2":"#FFF"};
+    font-size: 14px;
+    font-weight: 700;
+`
