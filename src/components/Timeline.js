57e0628ea649083a17cf1aac1b4b2a2ae15887a1
Luiz Fernando <lfaires@gmail.com>
2021-06-01 11:23:30 -0300

    setup da lib infinite-scroller

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import InfiniteScroll from 'react-infinite-scroller';
 import { useState, useContext, useEffect } from 'react';
 import { Container, Posts, Trending, Load } from "../styledComponents/Content";
 import Navbar from './Navbar';
@@ -18,7 +19,7 @@ export default function Timeline(){
 
     useEffect(() => {loadingPosts()},[])
 
-    function loadingPosts() {
+    function loadingPosts(page) {
         const localstorage = JSON.parse(localStorage.user);
         const token = localstorage.token;
         setIsLoading(true)
@@ -27,6 +28,7 @@ export default function Timeline(){
         const request = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts', config)
 
         request.then( response => {
+            console.log("objeto resp:",response)
             setUser(localStorage.user);
             const data = response.data.posts
             setPosts([...response.data.posts])
@@ -52,15 +54,17 @@ export default function Timeline(){
                         { isLoading ? <Load>Loading</Load> : ""}
                         { isError ? <Load>Houve uma falha ao obter os posts, <br/> por favor atualize a página</Load> : ""}
                         { isEmpty && !isLoading ? <Load>Nenhum post encontrado</Load> : ""}
-                        {posts.map( post => 
-                            <Post 
-                                key={post.id} id={post.id} post={post} 
-                                postUser={post.user} likes={post.likes}
-                                reloadingPosts={loadingPosts}
-                                location={location}
+                        <InfiniteScroll pageStart={0} hasMore>
+                            {posts.map( post => 
+                                <Post 
+                                    key={post.id} id={post.id} post={post} 
+                                    postUser={post.user} likes={post.likes}
+                                    reloadingPosts={loadingPosts}
+                                    location={location}
 
-                            />)
-                        }
+                                />)
+                            }
+                        </InfiniteScroll>
                     </Posts>
                     <Trending >
                         <TrendingBar />
