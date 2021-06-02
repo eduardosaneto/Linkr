6213a45cea66124990634bf87e56cda409c564a0
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-06-02 01:45:30 -0300

    Envio de requisição para o servidor para buscar usuários

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ import SearchBox from './SearchBox';
 export default function Navbar() {
 
     // const { user } = useContext(Usercontext);
-    const [search, setSearch] = useState("");
+    // const [search, setSearch] = useState("");
     const [showMenu, setShowMenu] = useState(0);
     const localstorage = JSON.parse(localStorage.user);
     const token = localstorage.token;
@@ -29,7 +29,7 @@ export default function Navbar() {
                 <Link to='/timeline'>
                     <h1>linkr</h1>
                 </Link>
-                <SearchBox />
+                {/* <SearchBox /> */}
                 {/* <Form>
                     <input 
                         type="search" placeholder="Search for people and friends"
@@ -41,7 +41,8 @@ export default function Navbar() {
                 </Form> */}
                 <div></div>
             </Header>
-            <ClickAwayListener onClickAway={() => setShowMenu(0)}>
+            <SearchBox />
+            <ClickAwayListener onClickAway={() => setShowMenu(0)}>                
                 <NavMenu >
                     <Menu direction={showMenu} avatar={image}>
                         <IoIosArrowDown className="arrow" onClick={toggleMenu}/>
@@ -66,43 +67,52 @@ export default function Navbar() {
     )
 }
 
-// const Form = styled.form`    
-//     position: relative;
-//     input {
-//         width: 563px;
-//         height: 45px;
-//         padding: 12px 0px 10px 17px;
-//         border: none;
-//         border-radius: 8px;
-//         background: #FFFFFF;
-//         font-size: 19px;
-//         line-height: 23px;
-//         color: #666;
-//         cursor: pointer;
-//     }
-//     button {
-//         height: 43px;
-//         border: none;
-//         border-radius: 8px;
-//         padding: 10px;
-//         position: absolute;
-//         top: 1px;
-//         right: 0;
-//         background: #fff;
-//     }
-//     .search {
-//         font-size: 26px;
-//         color: #C6C6C6
-//     }
-//     input::placeholder {
-//         color: #C6C6C6;
-//     }
-//     input:focus, button:focus {
-//         border: 0px solid #333;
-//         box-shadow: 0 0 0 0;
-//         outline: 0;
-//     }
-// `;
+const Header = styled.div`
+    width: 100%;
+    height: 72px;
+    padding: 10px 26px 10px 26px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #151515;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10;
+
+    h1 {
+        font-family: 'Passion One', sans-serif;
+        font-size: 49px;
+        letter-spacing: 2px;
+        line-height: 54px;
+        color: #fff;
+    }
+
+    > div {
+        width: 110px;
+        height: 53px;
+    }
+
+    @media (max-width: 611px){
+        padding-left: 17px;
+
+        h1{
+            font-size: 45px;
+        }
+    }
+`;
+
+const NavMenu = styled.div`
+    width: 132px;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    right: 0;
+    z-index: 10;
+`;
 
 const Menu = styled.div`
     width: 100%;
@@ -129,6 +139,7 @@ const Menu = styled.div`
         background-image: url("${props => props.avatar}");
         background-size: cover;
         background-position: center;
+        cursor: pointer;
     }
     @media (max-width: 611px){
         > div{
@@ -172,49 +183,44 @@ const Links = styled.div`
     }
 `;
 
-const NavMenu = styled.div`
-    width: 132px;
-    height: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    position: fixed;
-    top: 0;
-    right: 0;
-    z-index: 10;
-`;
+// const Form = styled.form`    
+//     position: relative;
+//     input {
+//         width: 563px;
+//         height: 45px;
+//         padding: 12px 0px 10px 17px;
+//         border: none;
+//         border-radius: 8px;
+//         background: #FFFFFF;
+//         font-size: 19px;
+//         line-height: 23px;
+//         color: #666;
+//         cursor: pointer;
+//     }
+//     button {
+//         height: 43px;
+//         border: none;
+//         border-radius: 8px;
+//         padding: 10px;
+//         position: absolute;
+//         top: 1px;
+//         right: 0;
+//         background: #fff;
+//     }
+//     .search {
+//         font-size: 26px;
+//         color: #C6C6C6
+//     }
+//     input::placeholder {
+//         color: #C6C6C6;
+//     }
+//     input:focus, button:focus {
+//         border: 0px solid #333;
+//         box-shadow: 0 0 0 0;
+//         outline: 0;
+//     }
+// `;
 
-const Header = styled.div`
-    width: 100%;
-    height: 72px;
-    padding: 10px 26px 10px 26px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background: #151515;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 10;
 
-    h1 {
-        font-family: 'Passion One', sans-serif;
-        font-size: 49px;
-        letter-spacing: 2px;
-        line-height: 54px;
-        color: #fff;
-    }
 
-    > div {
-        width: 110px;
-        height: 53px;
-    }
 
-    @media (max-width: 611px){
-        padding-left: 17px;
-
-        h1{
-            font-size: 45px;
-        }
-    }
-`;
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import axios from 'axios';
 import { IoIosSearch } from "react-icons/io";
 import styled from 'styled-components';
+import {DebounceInput} from 'react-debounce-input';
 
 export default function SearchBox() {
 
@@ -8,22 +10,65 @@ export default function SearchBox() {
     const token = localstorage.token;
     const [search, setSearch] = useState("");
 
+    function searchUser(e) {        
+        setSearch(e);
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`,                  
+            },
+            params: {
+                username: {search}
+                },
+            };
+        const request = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/search`, config);
+        request.then(response => {
+            alert("deu bom");
+            console.log(response.data);
+        });
+        request.catch(error => {
+            alert("deu ruim");
+            console.log(error);
+        });
+    }
+
     return (
         <Form>
-            <input 
-                type="search" placeholder="Search for people and friends"
-                value={search} onChange={(e) => setSearch(e.target.value)}
+            <DebounceInput 
+                type="search" placeholder="Search for people and friends" className="search-box"
+                minLength={3} debounceTimeout={300}
+                value={search} onChange={(e) => searchUser(e.target.value)}
             />
             <button type="submit">
             <IoIosSearch className="search"/>  
-            </button>            
+            </button>         
+            <Suggestions>
+                <li>
+                    Enter
+                </li>
+            </Suggestions> 
         </Form>
     );
 }
 
-const Form = styled.form`    
+const Suggestions = styled.ul`
+    width: 100%;
+    background-color: red;
+`;
+
+const Form = styled.div`    
+    width: 563px;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    border: none;
+    border-radius: 8px 8px 0 0;
     position: relative;
-    input {
+    position: fixed;
+    top: 13px;
+    left: calc(50% - 281px);
+    z-index: 10;
+    background-color: blue;
+    .search-box {
         width: 563px;
         height: 45px;
         padding: 12px 0px 10px 17px;
@@ -32,8 +77,15 @@ const Form = styled.form`
         background: #FFFFFF;
         font-size: 19px;
         line-height: 23px;
-        color: #666;
-        cursor: pointer;
+        color: #666;        
+    }
+    input::placeholder {
+        color: #C6C6C6;
+    }
+    input:focus, button:focus {
+        border: 0px solid #333;
+        box-shadow: 0 0 0 0;
+        outline: 0;
     }
     button {
         height: 43px;
@@ -47,14 +99,19 @@ const Form = styled.form`
     }
     .search {
         font-size: 26px;
-        color: #C6C6C6
-    }
-    input::placeholder {
         color: #C6C6C6;
+        cursor: pointer;
     }
-    input:focus, button:focus {
-        border: 0px solid #333;
-        box-shadow: 0 0 0 0;
-        outline: 0;
+    /* @media(max-width: 900px){
+        .search-box {
+            width: 250px;
+        }
     }
+    @media(max-width: 611px){
+        .search-box {
+            width: 100%;
+            max-width: 350px;
+            margin: 0 10px;
+        }
+    } */
 `;
