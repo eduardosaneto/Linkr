1c6af1848f8ce7c7c8cd1902a3cfa010217d8232
Luiz Fernando <lfaires@gmail.com>
2021-06-04 16:40:44 -0300

    bug do repost na atualização

--- src/components/Post.js
+++ src/components/Post.js
@@ -238,6 +238,7 @@ export default function Post({
     };
     const request = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts/${post.id}/share`,{},config);
     
+    request.then(reloadingPosts)
   }
 
   return (
